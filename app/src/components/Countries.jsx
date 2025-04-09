9bba3539870b3c213937402d44176977459310a4
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-09 22:03:45 +0200

    suite react project

--- app/src/components/Countries.jsx
+++ app/src/components/Countries.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import Card from "../components/Card"
 
 const Countries = () => {
     //Le useEffect se joue lorsque le composant est monté
-    const [data, setData]=useState([]);
+    const [data, setData] = useState([]);
     useEffect(() => {
         axios.get('https://restcountries.com/v3.1/all').then((res) => setData(res.data))
 
@@ -11,6 +12,17 @@ const Countries = () => {
     return (
         <div>
             <h1 className="text-3xl text-center">COUNTRIES</h1>
+            <ul className="flex flex-wrap gap-3 mx-auto w-100vw justify-around">
+                {/* {
+                    data.map((country, index) => (<li key={index}>{country.translations.fra.common}</li>)
+                    )
+                    } */}
+                    {
+                        data.map((country, index)=>(
+                            <Card key={index} country={country} />
+                        ))
+                    }
+            </ul>
         </div>
     );
 };
